b121b2b3442b7ca6a2058bdcf75fc3fc301e5301
Yusuf Pekşen <ahmetpeksen77@gmail.com>
2025-03-07 09:52:55 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  useParams,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n";
-
-function RedirectComponent() {
-  const { shortCode } = useParams();
-
-  if (shortCode) {
-    window.location.replace(
-      `https://sour-caitrin-yusufpeksen-e967d44c.koyeb.app/${shortCode}`
-    );
-  }
-
-  return null;
-}
 
 const router = createBrowserRouter([
   {
@@ -31,6 +15,12 @@ const router = createBrowserRouter([
     element: <RedirectComponent />,
   },
 ]);
+
+function RedirectComponent() {
+  const shortCode = window.location.pathname.substring(1);
+  window.location.href = `https://url-shortener-frontend-phi.vercel.app/${shortCode}`;
+  return null;
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
